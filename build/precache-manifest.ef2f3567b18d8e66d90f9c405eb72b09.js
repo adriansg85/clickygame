9b888be602bf053a8467a64db62f877be731e30a
adriansg85 <adriansalinasg@gmail.com>
2019-02-15 21:37:10 -0600

    app deployed

---
+++ build/precache-manifest.ef2f3567b18d8e66d90f9c405eb72b09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "dc0ff9c5a96909dc4caf",
+    "url": "/clickygame/static/js/main.dc0ff9c5.chunk.js"
+  },
+  {
+    "revision": "ecddac34e8b105dff208",
+    "url": "/clickygame/static/js/1.ecddac34.chunk.js"
+  },
+  {
+    "revision": "dc0ff9c5a96909dc4caf",
+    "url": "/clickygame/static/css/main.2d549916.chunk.css"
+  },
+  {
+    "revision": "404f767c8008f6a03faafe87d387cf31",
+    "url": "/clickygame/index.html"
+  }
+];
